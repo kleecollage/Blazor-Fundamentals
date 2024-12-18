984fe6d5eedc3c9e325e23ea7521f9d631853524
Klee Collage <klee.collage@gmail.com>
2024-12-18 16:41:20 -0600

    Render Fragments

---
+++ Fundamentals/Models/Address.cs
@@ -0,0 +1,9 @@
+namespace Fundamentals.Models;
+
+public class Address
+{
+    public string TypeAdress { get; set; }
+    public string Street { get; set; }
+    public int Code { get; set; }
+    public string City { get; set; }
+}
---
+++ Fundamentals/Models/Person.cs
@@ -0,0 +1,8 @@
+namespace Fundamentals.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public List<Address> Addresses { get; set; }
+}
